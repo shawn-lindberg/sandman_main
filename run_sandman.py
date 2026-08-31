b077624e434b08e32c61a3655001a0bedeecce4f
Shawn Lindberg <shawn.lindberg@gmail.com>
2026-08-30 21:47:15 -0500

    Got a very basic REST API mocked up and working.

--- run_sandman.py
+++ run_sandman.py
@@ -1,24 +1,6 @@
 """Main entry point for the Sandman application."""
 
-import time
-
 import sandman_main
-import sandman_main.sandman
 
 if __name__ == "__main__":
-    sandman = sandman_main.sandman.create_app()
-
-    if sandman is None:
-        raise ValueError("Failed to create Sandman application.")
-
-    sandman.start()
-
-    try:
-        while True:
-            # Sleep for 10 ms.
-            time.sleep(0.01)
-
-    except KeyboardInterrupt:
-        pass
-
-    sandman.stop()
+    app = sandman_main.create_app()
--- sandman_main/__init__.py
+++ sandman_main/__init__.py
@@ -1,1 +1,55 @@
 """The main application for Sandman."""
+
+import threading
+import time
+import typing
+
+import fastapi
+import uvicorn
+
+from . import sandman
+
+
+def _api_thread_run(app: fastapi.FastAPI) -> None:
+    """Run the API thread."""
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
+
+
+def create_app(
+    test_config: dict[typing.Any, typing.Any] | None = None,
+) -> None:
+    """Create and configure the app.
+
+    test_config - Which testing configuration to use, if any.
+    """
+    # I think this should become a part of the Sandman app.
+    app = fastapi.FastAPI()
+
+    @app.get("/health")
+    def get_health() -> dict[str, str]:
+        return {"health": "Healthy"}
+
+    api_thread = threading.Thread(
+        target=_api_thread_run, args=[app], daemon=True
+    )
+    api_thread.start()
+
+    sandman_app = sandman.create_app()
+
+    if sandman_app is None:
+        raise ValueError("Failed to create Sandman application.")
+
+    # I think we should be returning the app here, and starting it outside.
+    sandman_app.start()
+
+    try:
+        while True:
+            # Sleep for 10 ms.
+            time.sleep(0.01)
+
+    except KeyboardInterrupt:
+        pass
+
+    sandman_app.stop()
+
+    return None
