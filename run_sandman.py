f973174451579607d7afe28a4a393c77819f0b2b
Shawn Lindberg <shawn.lindberg@gmail.com>
2026-09-03 21:26:47 -0500

    Moved the run logic back into run_sandman.py.

--- run_sandman.py
+++ run_sandman.py
@@ -1,6 +1,20 @@
 """Main entry point for the Sandman application."""
 
+import time
+
 import sandman_main
 
 if __name__ == "__main__":
-    app = sandman_main.create_app()
+    sandman_app = sandman_main.create_app()
+
+    sandman_app.start()
+
+    try:
+        while True:
+            # Sleep for 10 ms.
+            time.sleep(0.01)
+
+    except KeyboardInterrupt:
+        pass
+
+    sandman_app.stop()
--- sandman_main/__init__.py
+++ sandman_main/__init__.py
@@ -1,6 +1,5 @@
 """The main application for Sandman."""
 
-import time
 import typing
 
 from . import sandman
@@ -8,7 +7,7 @@ from . import sandman
 
 def create_app(
     test_config: dict[typing.Any, typing.Any] | None = None,
-) -> None:
+) -> sandman.Sandman:
     """Create and configure the app.
 
     test_config - Which testing configuration to use, if any.
@@ -18,17 +17,4 @@ def create_app(
     if sandman_app is None:
         raise ValueError("Failed to create Sandman application.")
 
-    # I think we should be returning the app here, and starting it outside.
-    sandman_app.start()
-
-    try:
-        while True:
-            # Sleep for 10 ms.
-            time.sleep(0.01)
-
-    except KeyboardInterrupt:
-        pass
-
-    sandman_app.stop()
-
-    return None
+    return sandman_app
